05eea387b5c2925fd3855f015ea8258ae698b592
Sahil <arankallesahil@outlook.com>
2024-10-17 20:02:42 +0530

    PIC Commit

---
+++ SevenSeg/SevSeg.X/main.c
@@ -0,0 +1,26 @@
+Game Plan and Arena Specifications
+
+•	A timing system may be used to officially track the time of each pilot throughout the track.
+	(Gameplay time)
+•	The decision of the judges will be final and no sort of disputes will be entertained.
+•	Pilots must stay within all prescribed flight paths if violated equivalent action would be taken.
+•	Pilots must adhere to the prescribed launch sequence. No movement before the starting signal.
+•	Arena Brief will be provided and explained on the spot.
+
+Game Point 
+
+•	Drone must carry a payload and drop in the point region located at the end point.
+•	Payload will be a golf ball (or similar Object).
+•	The Drone shall then take off and travel a predefined circuit and drop its payload at the designated payload drop area that will be clearly marked on the ground.
+
+
+
+Safety Rules
+
+•	All drones must undergo a pre-race safety inspection by event organizers.
+•	Participants must wear appropriate safety gear.
+•	Pilots must adhere to all rules within the competition venue, and will not fly in any other part.
+•	Pilots must arrive at the venue with their complete setup 1 hour before the official race time and must be in complete Ready-To-Go state 15 mins before race time.
+•	Pilots must not assume any change of race time until the organizers officially announce it.
+•	Spectators must maintain a safe distance from the race area, as designated by event organizers.
+•	Any form of cheating, interference with other drones, or unsportsmanlike behavior will result in disqualification.
